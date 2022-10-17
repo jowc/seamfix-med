c7970c5854877daab401d1cda01685514e2bed37
jowc <jchikeme@gmail.com>
2022-10-17 21:55:33 +0100

    Started Smartmed project and installed dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultsComponent } from './defaults/defaults.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DefaultsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
