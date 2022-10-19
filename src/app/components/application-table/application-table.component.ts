08b259a394f8c500b188a4b55ef9e37b725ce983
jowc <jchikeme@gmail.com>
2022-10-19 07:44:37 +0100

    Added generic styles to server and application page

--- src/app/components/application-table/application-table.component.ts
+++ src/app/components/application-table/application-table.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationTableComponent implements OnInit {
 
+  options: string[] = ['All Application', 'All Application 1', 'All Application 2']
+  options2: string[] = ['All server', 'server 1', 'server 2']
+  options3: string[] = ['All status', 'Healthy', 'Critical']
+
   constructor() { }
 
   ngOnInit(): void {
